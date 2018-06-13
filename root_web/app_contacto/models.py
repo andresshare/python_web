fbfec4c5d25d11d77a2e437cb009526baa3c39d2
andresshare <andresshare@gmail.com>
2018-06-12 21:49:02 -0500

    Models

--- root_web/app_contacto/models.py
+++ root_web/app_contacto/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Moldel):
+    top_name = models.CharField(max_length=264, Unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    Topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264, Unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
